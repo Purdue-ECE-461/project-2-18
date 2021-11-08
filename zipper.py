c331c3c627831156aedeab27bcd8a40c1f6aac86
ander978 <54597740+ander978@users.noreply.github.com>
2021-11-08 18:53:41 -0500

    Add files via upload

---
+++ zipper.py
@@ -0,0 +1,9 @@
+from zipfile import ZipFile
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    file = "PA3.zip"
+    with ZipFile(file, 'r') as zipper:
+        zipper.extractall()
+
+
