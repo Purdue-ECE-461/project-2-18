7059435f74424552c79af2eaebf9542d4b9e1396
Kshaunish <kshaunishsoni123@gmail.com>
2021-11-13 14:22:01 -0500

    firestore add functionality

--- firestore/jsonFirestore.js
+++ firestore/jsonFirestore.js
@@ -1,46 +1,71 @@
-// This import loads the firebase namespace.
 const firebase = require("firebase");
-// Required for side-effects
 require("firebase/firestore");
+const fs = require('fs');
+const {resolve} = require('path');
 
-// Initialize Cloud Firestore through Firebase
 firebase.initializeApp({
     apiKey: "AIzaSyAmBHC0o_DLr6KcOvsjt15dXVZe5idDQB0",
     authDomain: "project-group18.firebaseapp.com",
-    databaseURL: "https://project-group18-default-rtdb.firebaseio.com/",
-    projectId: "project-group18"
-  });
-  
-var db = firebase.firestore();
-
-var menu =[  
-    {  
-       "id":1,
-       "name":"Focaccia al rosmarino",
-       "description":"Wood fired rosemary and garlic focaccia",
-       "price":8.50,
-       "type":"Appetizers"
-    },
-    {  
-       "id":2,
-       "name":"Burratta con speck",
-       "description":"Burratta cheese, imported smoked prok belly prosciutto, pached balsamic pear",
-       "price":13.50,
-       "type":"Appetizers"
-    }
- ]
-
-menu.forEach(function(obj) {
-    db.collection("menu").add({
-        id: obj.id,
-        name: obj.name,
-        description: obj.description,
-        price: obj.price,
-        type: obj.type
-    }).then(function(docRef) {
-        console.log("Document written with ID: ", docRef.id);
-    })
-    .catch(function(error) {
-        console.error("Error adding document: ", error);
-    });
+    projectId: "project-group18",
 });
+
+
+class addJsonFirestore {
+    constructor() {
+        this.db = firebase.firestore();
+        const [, , filepath, collectionName] = process.argv;
+        this.absolutePath = resolve(process.cwd(), filepath);
+        this.collection = collectionName;
+        //only add items from JSON file
+        //check if inputs are correct
+        if(this.absolutePath == NULL || this.absolutePath.length < 1){
+            console.error('File path error, ${this.absolutePath}');
+            this.exit(1);
+        }
+        if(this.collection == NULL || this.collection.length < 1){
+            console.error('collection namee error, ${this.collection}');
+            this.exit(1);
+        }
+
+    }
+
+    async populate(){
+        let data = [];
+        try{
+            data = JSON.parse(fs.readFileSync(this.absolutePath, {}), 'utf8');
+        } catch(e){
+            console.error(e.message);
+        }
+
+        if(data.length < 1){
+            console.error('make sure JSON file has data');
+        }
+        var i = 0;
+        for(var item in data){
+            console.log(item)
+            try{
+                this.add(item);
+            }catch(e){
+                console.error(e.message);
+            }
+            if(i == data.length - 1){
+                console.log('UPLOAD SUCCESS');
+                this.exit(0);
+            }  
+            i++;          
+        }
+
+    }
+
+    add(item){
+        console.log('adding item${item.id}');
+        return this.db.collection(this.collection).add(Object.assign({}, item))
+        .then(() => true)
+        .catch((e) => console.error(e.message));
+
+    }
+
+    exit(code){
+        return process.exit(code);
+    }
+}
