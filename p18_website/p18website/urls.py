b4fb109541e30234e7b8c557c1bb23107b79c205
GitHub Action <action@github.com>
2021-12-01 22:10:08 +0000

    isort

--- p18_website/p18website/urls.py
+++ p18_website/p18website/urls.py
@@ -14,11 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
 from django.contrib.auth import views as authviews
+from django.urls import path
 
 from . import views
-
 
 urlpatterns = [
     path('admin/', admin.site.urls),
