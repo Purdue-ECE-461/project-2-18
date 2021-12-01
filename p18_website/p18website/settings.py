4f6828a91e01e5845d4b363f68ab3e3ca316ffd5
ander978 <54597740+ander978@users.noreply.github.com>
2021-12-01 17:06:25 -0500

    Update settings.py

--- p18_website/p18website/settings.py
+++ p18_website/p18website/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-w^_go6x_d@qn0x%51e3z5t#)^o7p9=5pqvq+tt1=xu_zu*$hw#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-#ALLOWED_HOSTS = []
+# ALLOWED_HOSTS = []
 
 
 # Application definition
