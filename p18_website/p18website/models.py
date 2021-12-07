cbf02de7c2a5fa660ced45247725be3463bb5c07
Kshaunish <kshaunishsoni123@gmail.com>
2021-12-07 14:08:24 -0500

    POST complete

--- p18_website/p18website/models.py
+++ p18_website/p18website/models.py
@@ -4,3 +4,5 @@ class Package(models.Model):
     name = models.CharField(max_length = 200, blank=True, default='')
     version = models.CharField(max_length = 200, blank=True, default='')
     url = models.CharField(max_length = 200, blank=True, default='')
+    content = models.CharField(max_length = 1000, blank=True, default='')
+    JSProgram = models.CharField(max_length = 200, blank=True, default='')
