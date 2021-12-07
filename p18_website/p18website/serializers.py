49761d05187abbdacf49b7f8ab4aaaafa9b96ba1
Lazywave <64357146+Lazywave@users.noreply.github.com>
2021-12-07 15:37:16 -0500

    Comment currently unused imports

--- p18_website/p18website/serializers.py
+++ p18_website/p18website/serializers.py
@@ -1,6 +1,6 @@
 from .models import Package
 from rest_framework import serializers
-from django.contrib.auth.models import User
+# from django.contrib.auth.models import User
 
 
 class PackageSerializer(serializers.ModelSerializer):
--- p18_website/p18website/views.py
+++ p18_website/p18website/views.py
@@ -2,9 +2,9 @@
 #from django.template import loader"""
 from django.shortcuts import render
 from rest_framework import generics
-from rest_framework import mixins
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
+# from rest_framework import mixins
+# from rest_framework.decorators import api_view
+# from rest_framework.response import Response
 from .models import Package
 from .serializers import PackageSerializer, RatingSerializer
 
