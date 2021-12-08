ac15181725b9fe69a1032e280c15516b3f718181
Lazywave <64357146+Lazywave@users.noreply.github.com>
2021-12-07 19:25:12 -0500

    Fix imports

--- p18_website/p18website/urls.py
+++ p18_website/p18website/urls.py
@@ -13,12 +13,12 @@ Including another URLconf
     1. Import include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+# from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.conf.urls import url
 from django.contrib import admin
 from django.contrib.auth import views as authviews
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.urls import path
-from p18website import views
+import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
