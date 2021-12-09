04868be4028e6d44dc109f3a2fdfe3ff59e4e60a
Kshaunish <kshaunishsoni123@gmail.com>
2021-12-08 22:27:28 -0500

    changes to settings.py

--- p18_website/p18website/settings.py
+++ p18_website/p18website/settings.py
@@ -31,7 +31,7 @@ elif os.environ.get("GOOGLE_CLOUD_PROJECT", None):
     project_id = os.environ.get("GOOGLE_CLOUD_PROJECT")
 
     client = secretmanager.SecretManagerServiceClient()
-    settings_name = os.environ.get("SETTINGS_NAME", "django_settings")
+    settings_name = os.environ.get('DJANGO_SETTINGS_MODULE', 'p18website.settings')
     name = f"projects/{project_id}/secrets/{settings_name}/versions/latest"
     payload = client.access_secret_version(name=name).payload.data.decode("UTF-8")
 
